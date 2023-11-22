2987e92fc2d7f9f1e974c4c18344baea6952f3da
Eoghan Murray <eoghan@getthere.ie>
2023-11-22 10:45:57 +0000

    fixup! Cleanup: systematically replace the rest of the html entities with their unicode equivalent

--- transform.py
+++ transform.py
@@ -5,8 +5,11 @@ for fn in glob('*.htm') + ['index.html']:
     with open(fn, 'r') as f:
         content = f.read()
         ncontent = content
-        ncontent = ncontent.replace('&quot;', '"')
-        ncontent = ncontent.replace('&copy;', '©')
+        for k, v in html.entities.html5.items():
+            if not k.endswith(';'):
+                k = k + ';'
+            if k not in ['nbsp;', 'gt;', 'lt;', 'amp;']:
+                ncontent = ncontent.replace('&'+k, v)
         ncontent = ncontent.replace('(c) L', '© L')
         ncontent = ncontent.replace('(C) L', '© L')
         if content != ncontent:
